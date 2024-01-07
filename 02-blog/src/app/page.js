491c1ca2834f369e6f8645fd3551e067e0e0574f
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-07 11:11:02 +0530

    Events, Functions and state

--- 02-blog/src/app/page.js
+++ 02-blog/src/app/page.js
@@ -1,16 +1,20 @@
 'use client' //must import use client directive for events run on client side server
 import styles from './page.module.css'
+import { useState } from 'react';
 export default function Home() {
+  // state
+  const [name1, setname1] = useState('My name is Shri');
 
   // Function
-  const name = ()=>{
-    alert('hello, my name is shri')
+  const change = ()=>{
+    setname1('My name is Shrikant')
   }
+ 
   return (
     <main className={styles.main}>
-     <h2>Events, Functions and state</h2>
+     <h2>{name1}</h2>
      {/* On click id a Event */}
-     <button onClick={name}>Click Me</button>
+     <button onClick={change}>Click Me</button>
     </main>
   )
 }
