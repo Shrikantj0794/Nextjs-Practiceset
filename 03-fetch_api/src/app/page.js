2af78ef05a767635787be029dfb341eab4083a40
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-12 20:12:31 +0530

    apply condition on HTML tag

--- 03-fetch_api/src/app/page.js
+++ 03-fetch_api/src/app/page.js
@@ -5,14 +5,19 @@ import { useState } from 'react'
 
 export default function Home() {
   const [colore, setColore] = useState("red");
-
+  const print = 'true';
   return (
 
     <main className='main'>
       <Link href='/productlist'> Go To Product page client side </Link>
       <Link href='/2productlist'> Go To Product page server side </Link>
-      <h1 className={colore=='red'?Custom.red:Custom.green}>Hello, My name is Shrikant</h1>
+      <h1 className={colore=='red'?Custom.red:Custom.green}>My name is Shrikant</h1>
       <button onClick={()=>setColore("green")}>change Colore</button>
+
+      <h1 style={colore=="red"? {color:'red'}:{color:'blue'}}>My name is Shri</h1>
+      <button onClick={()=>setColore({color:'blue'})}>change Colore</button>
+
+      {print=='true'? <h1>HI</h1>: <h1>Hello</h1> }
     </main>
   )
 }
