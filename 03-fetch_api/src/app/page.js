d47b244a6bdf54ca75488e7692f09053c207aa0e
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-12 16:17:37 +0530

    use css module

--- 03-fetch_api/src/app/page.js
+++ 03-fetch_api/src/app/page.js
@@ -1,13 +1,18 @@
 "use client"
+import Custom from './custom.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
+
 export default function Home() {
   const [change, setchange] = useState({backgroundColor:"red"});
+
   return (
+
     <main className='main'>
-      <Link href='/productlist' style={change}> Go To Product page client side </Link>
+      <Link href='/productlist' style={change} > Go To Product page client side </Link>
       <button onClick={()=>setchange({backgroundColor:"blue"})}>Change Colore</button>
       <Link href='/2productlist'> Go To Product page server side </Link>
+      <h1 className={Custom.h1}>hello My name is shri</h1>
     </main>
   )
 }
