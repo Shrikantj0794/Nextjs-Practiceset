888ec6bc6bc78751c66bc2e32c3c1b5ab8ac53b4
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-13 21:54:53 +0530

    use Font optimization in next.js

--- 05-use_font/src/app/page.js
+++ 05-use_font/src/app/page.js
@@ -1,11 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import { Roboto } from "next/font/google"
+const roboto = Roboto({
+  weight: '100',
+  subsets:['latin'],
+  display: "swap"
+})
 export default function Home() {
   return (
     <div className='main'>
       <h1 style={{fontFamily: 'Lemon', fontWeight:100}}>Font with Link Tag in Next</h1>
-      <h1>Font optimization in Next</h1>
+      <h1 className={roboto.className}>Font optimization in Next</h1>
     </div>
   )
 }
