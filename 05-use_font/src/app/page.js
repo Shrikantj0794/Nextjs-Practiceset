14ae1bab860c07146d3ecbb93d2f983a105a75f9
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-13 21:47:47 +0530

    External font using link tag in next

---
+++ 05-use_font/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className='main'>
+      <h1 style={{fontFamily: 'Lemon', fontWeight:100}}>Font with Link Tag in Next</h1>
+      <h1>Font optimization in Next</h1>
+    </div>
+  )
+}
