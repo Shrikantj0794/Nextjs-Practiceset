3edf9131734fbf9820139c109df9c17024e8a3d7
Shrikant Jamale <shrikantj0794@gmail.com>
2024-01-13 22:39:45 +0530

    change title page wise using generateMetadata

--- 05-use_font/src/app/page.js
+++ 05-use_font/src/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { Roboto } from "next/font/google"
 const roboto = Roboto({
   weight: '100',
@@ -9,6 +10,7 @@ export default function Home() {
     <div className='main'>
       <h1 style={{fontFamily: 'Lemon', fontWeight:100}}>Font with Link Tag in Next</h1>
       <h1 className={roboto.className}>Font optimization in Next</h1>
+      <Link href='/user'>Go to User Page</Link>
     </div>
   )
 }
---
+++ 05-use_font/src/app/user/page.js
@@ -0,0 +1,13 @@
+export default function page(){
+    return(
+        <div>
+            <h1>Check Title changed</h1>
+        </div>
+    )
+}
+
+export function generateMetadata(){
+    return{
+        title: 'User Page'
+    }
+}
